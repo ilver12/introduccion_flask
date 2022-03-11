0bd4ced8b3d5a4bef33b95f3818686a0b5725d1b
ilver villarreal <ediililve2001@gmail.com>
2022-03-11 18:41:05 -0500

    Nuevos cambios

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from distutils.log import debug
 from pydoc import render_doc
 from sqlite3 import Cursor
+from turtle import update
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
@@ -28,13 +29,47 @@ def inicio():
 
     return render_template("index.html", productos=productos)
 
+@app.route("/eliminar/<int:id>")
+def eliminarProducto(id):
+    cursor = db.cursor()
+    cursor.execute("delete from productos where id={0}".format(id))
+    cursor.close()
+    return redirect(url_for('inicio'))
+
+@app.get("/editar")
+def productoEditar():
+    return render_template('editarProducto.html')
+
+@app.route('/editarProducto/<int:id>')
+def editarProducto(id):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM productos WHERE id = {0}".format(id))
+    item = cursor.fetchall()
+    cursor.close()
+    return render_template('editarProducto.html', producto = item[0])
+
+@app.route('/actualizarProducto/<id>', methods=['POST'])
+def actualizarProducto(id):
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        price = request.form['price']
+        cursor = db.cursor()
+        cursor.execute("UPDATE productos SET  nombre = %s, price = %s WHERE id = %s", 
+        (nombre, 
+        price,
+        id,
+        )) 
+        cursor.close()
+        return redirect(url_for('inicio'))
+
+
 @app.get("/form_crear")
 def formCrearProducto():
     return render_template("crearProducto.html")
 
 @app.post("/form_crear")
 def crearProducto():
-    #recuperar los datos del formulario
+    #recuperar los dato s del formulario
     nombre = request.form.get('nombre')
     price = request.form.get('price')
 
